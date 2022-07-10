648bd03f23d4cf342e0851645b6bcbe3b2789929
eperez045 <eperez045@gmail.com>
2022-07-10 14:48:04 +0200

    estilos

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
+
+
+
 function resultComp(){
     let result = (Math.floor(Math.random() * 3));
     if (result == 0){
-        result = "rock"
+        result = "rock";
+        document.getElementById('a').innerHTML='<img class="image" src="./img/rock.png" />'
+
     }else if(result == 1){
         result = "paper"
+        document.getElementById('a').innerHTML='<img class="image" src="./img/paper.png" />'
+
     }else{
         result = "scissors"
+        document.getElementById('a').innerHTML='<img class="image" src="./img/scissors.png" />'
     }
     return result
 }
-console.log(resultComp())
+resultComp()
